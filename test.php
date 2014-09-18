42ad4d8484e8d328b97dd20e4d054e5e840c1998
tony <tony@dev>
2014-09-18 16:55:34 +0000

    new gateway

--- test.php
+++ test.php
@@ -1,9 +1,24 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$username = 'sa';
+$pw = 'HTG2013';
+$dbname = 'acsys_db';
+$hostname = '10.3.0.46';
+$data = array();
 
-echo substr(PHP_OS, 0, 3);
+$table = 'keyUsers';
+
+    $conn = new PDO("dblib:host=$hostname ; dbname=$dbname","$username","$pw");
+    
+    $smt = $conn->prepare("select right(rtrim(ltrim(telephone)),9) as telephone FROM keyUsers where len(telephone) > 5");
+    
+    $re = $smt->execute();
+    
+    foreach($re as $record)
+    {
+        $msg = "Hello, you can now get your HTG ACSYS key code by sending 5m (for 5 minutes) or 8h (for 8 minutes ) to 0270300362";
+        echo 'sending to '.$record['telephone']."\n";
+        file_get_contents('http://10.3.0.13/smsgateway/api.php?to='.$to.'&msg='.$msg);
+        
+        
+    }
